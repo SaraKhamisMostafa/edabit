28c71a96026a188b97e5bcedb2d1c6a7d9a79849
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-03-12 19:50:59 +0200

    add edabit practices

---
+++ How Edabit Works/How Edabit Works.py
@@ -0,0 +1,13 @@
+'''
+All you have to do is type return "hello edabit.com" on the second line and then click the
+Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL.
+Click it and see what happens.
+
+Notes
+The returned string must be in all lowercase letters.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+'''
+
+def hello():
+    return "hello edabit.com"
